3c70e5ed9a75f7abddc76e449ff1edc69b8c5a71
Jonas Jensen <jje@netcompany.com>
2017-11-14 13:32:06 +0100

    Footer small, medium and big - done.

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -6,31 +6,34 @@ const forEach = require('array-foreach');
 const select = require('../utils/select');
 
 const CLICK = require('../events').CLICK;
-const PREFIX = require('../config').prefix;
+const PREFIX = require('../config').prefix; //JJE: not used anymore 
 
 const HIDDEN = 'hidden';
-const SCOPE = `.${PREFIX}-footer-big`;
+const SCOPE = `.footer`; //${PREFIX}-
 const NAV = `${SCOPE} nav`;
-const BUTTON = `${NAV} .${PREFIX}-footer-primary-link`;
+const BUTTON = `${NAV} .footer-primary-link`; //${PREFIX}-
 const LIST = `${NAV} ul`;
 
 const HIDE_MAX_WIDTH = 600;
 const DEBOUNCE_RATE = 180;
 
 const showPanel = function () {
-  const list = this.closest(LIST);
-  list.classList.remove(HIDDEN);
+  const small_screen = window.innerWidth < HIDE_MAX_WIDTH;
+  if(small_screen){
+    const list = this.closest(LIST);
+    list.classList.remove(HIDDEN);
 
-  // NB: this *should* always succeed because the button
-  // selector is scoped to ".{prefix}-footer-big nav"
-  const lists = list.closest(NAV)
-    .querySelectorAll('ul');
+    // NB: this *should* always succeed because the button
+    // selector is scoped to ".{prefix}-footer-big nav"
+    const lists = list.closest(NAV)
+      .querySelectorAll('ul');
 
-  forEach(lists, el => {
-    if (el !== list) {
-      el.classList.add(HIDDEN);
-    }
-  });
+    forEach(lists, el => {
+      if (el !== list) {
+        el.classList.add(HIDDEN);
+      }
+    });
+  }
 };
 
 const resize = debounce(() => {
