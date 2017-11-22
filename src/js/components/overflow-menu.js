7b6019601379721d9fa38050d44976095d6d9fd0
sfa <sfa@netcompany.com>
2017-11-22 15:41:23 +0100

    Added overflow menu to datatable, fixed overflow menu.

--- src/js/components/overflow-menu.js
+++ src/js/components/overflow-menu.js
@@ -1,15 +1,16 @@
 var $ = require('jquery');
 
-$('.overflow-menu-btn').on('click', function () {
-    $('.overflow-menu-options').addClass('overflow-menu-options-open');
+$('.overflow-menu-btn').on('click', function (btn) {
+    $(this).next('.overflow-menu-options').toggleClass('overflow-menu-options-open');
 });
 
-$('.overflow-menu-btn').on('blur', function () {
-    $('.overflow-menu-options').removeClass('overflow-menu-options-open');
+$('.overflow-menu-btn').on('blur', function (btn) {
+    $(this).next('.overflow-menu-options').removeClass('overflow-menu-options-open');
 });
 
 // onclick doesnt work because onblur is fired first
 $('.overflow-menu-options-btn').on('mousedown', function () {
     console.log($(this).text() + ' clicked');
 });
+
 
