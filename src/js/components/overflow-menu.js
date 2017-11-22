ae15f7d075fdcc0ed45a8bcfcc63e10ab4746c04
sfa <sfa@netcompany.com>
2017-11-22 17:23:35 +0100

    Fixed overflow menu position in datatable, changed datatable width to 100%

--- src/js/components/overflow-menu.js
+++ src/js/components/overflow-menu.js
@@ -1,16 +1,23 @@
 var $ = require('jquery');
 
-$('.overflow-menu-btn').on('click', function (btn) {
+$('.overflow-menu-btn').on('click', function () {
     $(this).next('.overflow-menu-options').toggleClass('overflow-menu-options-open');
 });
 
-$('.overflow-menu-btn').on('blur', function (btn) {
-    $(this).next('.overflow-menu-options').removeClass('overflow-menu-options-open');
+$('.overflow-menu-btn').on('blur', function () {
+    $(this).next('.overflow-menu-options').removeClass('overflow-menu-options-open');        
 });
 
-// onclick doesnt work because onblur is fired first
-$('.overflow-menu-options-btn').on('mousedown', function () {
-    console.log($(this).text() + ' clicked');
+// Prevent button from losing focus when clicking option button
+$('.overflow-menu-options-btn').on('mousedown', function (event) {
+    event.preventDefault();
 });
 
+// Fire losing button focus after clicking option button
+$('.overflow-menu-options-btn').on('mouseup', function () {
+    $('.overflow-menu-btn').blur();        
+});
 
+$('.overflow-menu-options-btn').on('click', function () {
+    console.log($(this).text() + ' clicked');  
+});
