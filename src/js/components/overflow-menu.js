919651ec9118ab910e7602f917910266f5fb8fda
sfa <sfa@netcompany.com>
2017-12-05 16:35:25 +0100

    Added primary button, fixed row hover and overflow menu

--- src/js/components/overflow-menu.js
+++ src/js/components/overflow-menu.js
@@ -2,13 +2,14 @@ var $ = require('jquery');
 
 // Handle hiding and showing overflow menu
 $(document).click(function(event) {
-    var isMenu = $(event.target).closest('.overflow-menu-options').length > 0;    
-    if (!isMenu) {
-        $('.overflow-menu-options').removeClass('open');
+    var menu = $(event.target).closest('.overflow-menu');    
+    if (menu.length == 0) {
+        $('.overflow-menu').removeClass('open');
     }
 
     var btn = $(event.target).closest('.overflow-menu-btn'); 
     if (btn.length > 0) {
-        btn.next('.overflow-menu-options').toggleClass('open');
+        $('.overflow-menu').not(menu).removeClass('open');
+        btn.closest('.overflow-menu').toggleClass('open');
     }
 });
