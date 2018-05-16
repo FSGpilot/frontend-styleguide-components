49120cc308290d5c85f01a7800948f1520293f1a
Emil Jensen <eje@netcompany.com>
2018-05-16 09:31:02 +0200

    Select 2 extra styling.

--- src/js/components-advanced/select2.js
+++ src/js/components-advanced/select2.js
@@ -19,37 +19,7 @@ module.exports = behavior(
       forEach(document.querySelectorAll(ELEMENT_SELECTOR), select2Element => {
         $(select2Element).select2({
           placeholder: "Vælg et element på listen",
-          width: "100%",
-          ajax: {
-            url: "http://sh-example-simple.herokuapp.com",
-            dataType: "json",
-            width: 'style',
-            delay: 250,
-            data: function(params) {
-              return {
-                q: params.term,
-                page: params.page,
-                per_page: 10
-              };
-            },
-            processResults: function(data, page) {
-              return {
-                // Select2 requires an id, so we need to map the results and add an ID
-                // You could instead include an id in the tsv you add to soulheart ;)
-                results: data.matches.map(function(item) {
-                  return {
-                    id: item.text,
-                    text: item.text
-                  };
-                }),
-                pagination: {
-                  // If there are 10 matches, there's at least another page
-                  more: data.matches.length === 10
-                }
-              };
-            },
-            cache: true
-          }
+          width: "100%"
         });
       });
     },
