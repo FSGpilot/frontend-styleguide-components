e110636e2ca6c5c12d2857bcd492229dded799ed
kasper Bremerskov <kasperbremer@hotmail.com>
2018-06-01 14:07:37 +0200

    disabled the modal.js

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -2,7 +2,7 @@ module.exports = {
   accordion:  require('./accordion'),
   banner:     require('./banner'),
   footer:     require('./footer'),
-  modal:     require('./modal'),
+  //modal:     require('./modal'),
   navigation: require('./navigation'),
   password:   require('./password'),
   search:     require('./search'),
