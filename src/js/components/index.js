5ce1bf0090e4fba38a8d8cb42a1fc438cba31ac8
Emil Jensen <eje@netcompany.com>
2018-07-11 15:48:48 +0200

    removed footer js (not used).

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,15 +1,10 @@
 module.exports = {
   accordion:  require('./accordion'),
-  banner:     require('./banner'),
-  footer:     require('./footer'),
   navigation: require('./navigation'),
   skipnav:    require('./skipnav'),
   validator:  require('./validator'),
   regexmask:  require('./regex-input-mask'),
   dropdown:   require('./dropdown'),
   navsubmenu:   require('./nav-submenu'),
-  collapse:   require('./collapse'),
-  tooltip:   require('./tooltip'),
-  //table:      require('./table'),
-  
+  collapse:   require('./collapse')
 };
